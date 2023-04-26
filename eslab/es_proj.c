f3b103ae4753e20b4a8b45f5ada31cc19d0fb13a
Anirudh R <64804815+Anirudh-R-2506@users.noreply.github.com>
2023-04-26 14:27:11 +0530

    Bug fixes

--- eslab/es_proj.c
+++ eslab/es_proj.c
@@ -6,61 +6,52 @@
 #define PEDRED (1<<19)
 #define PEDGREEN (1<<18)
 #define PEDBUTTON (1<<17)
-#define DELAY 6000000
 
-void EINT3_IRQHandler(void)
+void display_delay(){
+	unsigned int i;
+	for(i=0; i<6000000; i++);
+}
+
+void EINT3_IRQHandler(void)// pedestrian crossing
 {
-    // disable interrupts to avoid button debouncing
     NVIC_DisableIRQ(EINT3_IRQn);
-
-    // pedestrian crossing
     LPC_GPIO2->FIOPIN &= ~(GREEN | YELLOW);
     LPC_GPIO2->FIOPIN |= PEDRED;
-    for(int i=0; i<DELAY; i++);
+    display_delay();
     LPC_GPIO2->FIOPIN &= ~PEDRED;
     LPC_GPIO2->FIOPIN |= PEDGREEN;
-    for(int i=0; i<DELAY; i++);
+    display_delay();
     LPC_GPIO2->FIOPIN &= ~PEDGREEN;
     LPC_GPIO2->FIOPIN |= YELLOW;
-    for(int i=0; i<DELAY; i++);
+    display_delay();
     LPC_GPIO2->FIOPIN &= ~(YELLOW | PEDGREEN);
     LPC_GPIO2->FIOPIN |= GREEN;
 
-    // clear interrupt flag
     LPC_GPIOINT->IO2IntClr |= PEDBUTTON;
 
-    // enable interrupts again
     NVIC_EnableIRQ(EINT3_IRQn);
 }
 
 int main(void)
 {
-    // set pins as output
+	SystemInit();
+	SystemCoreClockUpdate();
     LPC_GPIO2->FIODIR |= RED | YELLOW | GREEN | PEDRED | PEDGREEN;
-
-    // set pin as input
     LPC_GPIO2->FIODIR &= ~PEDBUTTON;
-
-    // set green light as default
     LPC_GPIO2->FIOPIN |= GREEN;
-
-    // set external interrupt on falling edge of pin P2.17 (PEDBUTTON)
     LPC_GPIOINT->IO2IntEnF |= PEDBUTTON;
-
-    // enable interrupt for EINT3_IRQn (external interrupt 3)
     NVIC_EnableIRQ(EINT3_IRQn);
 
-    while(1)
+    while(1)// traffic light changes
     {
-        // automatic light changes
         LPC_GPIO2->FIOPIN &= ~(GREEN | YELLOW);
         LPC_GPIO2->FIOPIN |= RED;
-        for(int i=0; i<DELAY; i++);
+        display_delay();
         LPC_GPIO2->FIOPIN &= ~(RED | YELLOW);
         LPC_GPIO2->FIOPIN |= YELLOW;
-        for(int i=0; i<DELAY; i++);
+        display_delay();
         LPC_GPIO2->FIOPIN &= ~(RED | YELLOW);
         LPC_GPIO2->FIOPIN |= GREEN;
-        for(int i=0; i<DELAY; i++);
+        display_delay();
     }
 }
